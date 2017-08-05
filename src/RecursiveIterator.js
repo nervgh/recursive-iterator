b0189c397cdfd676d687de8bc647ef931326b028
nerv <nervgh@gmail.com>
2017-08-05 08:45:40 +0300

    we should follow standard code style

--- src/RecursiveIterator.js
+++ src/RecursiveIterator.js
@@ -1,138 +1,133 @@
-'use strict';
+'use strict'
 
-
-const {isObject,getKeys} = require('./lang');
-
+const {isObject, getKeys} = require('./lang')
 
 // PRIVATE PROPERTIES
-const BYPASS_MODE = '__bypassMode';
-const IGNORE_CIRCULAR = '__ignoreCircular';
-const MAX_DEEP = '__maxDeep';
-const CACHE = '__cache';
-const QUEUE = '__queue';
-const STATE = '__state';
+const BYPASS_MODE = '__bypassMode'
+const IGNORE_CIRCULAR = '__ignoreCircular'
+const MAX_DEEP = '__maxDeep'
+const CACHE = '__cache'
+const QUEUE = '__queue'
+const STATE = '__state'
 
-
-const EMPTY_STATE = {};
-
+const EMPTY_STATE = {}
 
 class RecursiveIterator {
-    /**
-     * @param {Object|Array} root
-     * @param {Number} [bypassMode=0]
-     * @param {Boolean} [ignoreCircular=false]
-     * @param {Number} [maxDeep=100]
-     */
-    constructor(root, bypassMode = 0, ignoreCircular = false, maxDeep = 100) {
-        this[BYPASS_MODE] = bypassMode;
-        this[IGNORE_CIRCULAR] = ignoreCircular;
-        this[MAX_DEEP] = maxDeep;
-        this[CACHE] = [];
-        this[QUEUE] = [];
-        this[STATE] = this.getState(undefined, root);
-    }
-    /**
-     * @returns {Object}
-     */
-    next() {
-        let {node, path, deep} = this[STATE] || EMPTY_STATE;
+  /**
+   * @param {Object|Array} root
+   * @param {Number} [bypassMode=0]
+   * @param {Boolean} [ignoreCircular=false]
+   * @param {Number} [maxDeep=100]
+   */
+  constructor (root, bypassMode = 0, ignoreCircular = false, maxDeep = 100) {
+    this[BYPASS_MODE] = bypassMode
+    this[IGNORE_CIRCULAR] = ignoreCircular
+    this[MAX_DEEP] = maxDeep
+    this[CACHE] = []
+    this[QUEUE] = []
+    this[STATE] = this.getState(undefined, root)
+  }
+  /**
+   * @returns {Object}
+   */
+  next () {
+    let {node, path, deep} = this[STATE] || EMPTY_STATE
 
-        if (this[MAX_DEEP] > deep) {
-            if (this.isNode(node)) {
-                if (this.isCircular(node)) {
-                    if (this[IGNORE_CIRCULAR]) {
+    if (this[MAX_DEEP] > deep) {
+      if (this.isNode(node)) {
+        if (this.isCircular(node)) {
+          if (this[IGNORE_CIRCULAR]) {
                         // skip
-                    } else {
-                        throw new Error('Circular reference');
-                    }
-                } else {
-                    if (this.onStepInto(this[STATE])) {
-                        let descriptors = this.getStatesOfChildNodes(node, path, deep);
-                        let method = this[BYPASS_MODE] ? 'push' : 'unshift';
-                        this[QUEUE][method](...descriptors);
-                        this[CACHE].push(node);
-                    }
-                }
-            }
+          } else {
+            throw new Error('Circular reference')
+          }
+        } else {
+          if (this.onStepInto(this[STATE])) {
+            let descriptors = this.getStatesOfChildNodes(node, path, deep)
+            let method = this[BYPASS_MODE] ? 'push' : 'unshift'
+            this[QUEUE][method](...descriptors)
+            this[CACHE].push(node)
+          }
         }
+      }
+    }
 
-        let value = this[QUEUE].shift();
-        let done = !value;
+    let value = this[QUEUE].shift()
+    let done = !value
 
-        this[STATE] = value;
+    this[STATE] = value
 
-        if (done) this.destroy();
+    if (done) this.destroy()
 
-        return {value, done};
-    }
-    /**
-     *
-     */
-    destroy() {
-        this[QUEUE].length = 0;
-        this[CACHE].length = 0;
-        this[STATE] = null;
-    }
-    /**
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    isNode(any) {
-        return isObject(any);
-    }
-    /**
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    isLeaf(any) {
-        return !this.isNode(any);
-    }
-    /**
-     * @param {*} any
-     * @returns {Boolean}
-     */
-    isCircular(any) {
-        return this[CACHE].indexOf(any) !== -1
-    }
-    /**
-     * Returns states of child nodes
-     * @param {Object} node
-     * @param {Array} path
-     * @param {Number} deep
-     * @returns {Array<Object>}
-     */
-    getStatesOfChildNodes(node, path, deep) {
-        return getKeys(node).map(key =>
-            this.getState(node, node[key], key, path.concat(key), deep + 1)
-        );
-    }
-    /**
-     * Returns state of node. Calls for each node
-     * @param {Object} [parent]
-     * @param {*} [node]
-     * @param {String} [key]
-     * @param {Array} [path]
-     * @param {Number} [deep]
-     * @returns {Object}
-     */
-    getState(parent, node, key, path = [], deep = 0) {
-        return {parent, node, key, path, deep};
-    }
-    /**
-     * Callback
-     * @param {Object} state
-     * @returns {Boolean}
-     */
-    onStepInto(state) {
-        return true;
-    }
+    return {value, done}
+  }
+  /**
+   *
+   */
+  destroy () {
+    this[QUEUE].length = 0
+    this[CACHE].length = 0
+    this[STATE] = null
+  }
+  /**
+   * @param {*} any
+   * @returns {Boolean}
+   */
+  isNode (any) {
+    return isObject(any)
+  }
+  /**
+   * @param {*} any
+   * @returns {Boolean}
+   */
+  isLeaf (any) {
+    return !this.isNode(any)
+  }
+  /**
+   * @param {*} any
+   * @returns {Boolean}
+   */
+  isCircular (any) {
+    return this[CACHE].indexOf(any) !== -1
+  }
+  /**
+   * Returns states of child nodes
+   * @param {Object} node
+   * @param {Array} path
+   * @param {Number} deep
+   * @returns {Array<Object>}
+   */
+  getStatesOfChildNodes (node, path, deep) {
+    return getKeys(node).map(key =>
+      this.getState(node, node[key], key, path.concat(key), deep + 1)
+    )
+  }
+  /**
+   * Returns state of node. Calls for each node
+   * @param {Object} [parent]
+   * @param {*} [node]
+   * @param {String} [key]
+   * @param {Array} [path]
+   * @param {Number} [deep]
+   * @returns {Object}
+   */
+  getState (parent, node, key, path = [], deep = 0) {
+    return {parent, node, key, path, deep}
+  }
+  /**
+   * Callback
+   * @param {Object} state
+   * @returns {Boolean}
+   */
+  onStepInto (state) {
+    return true
+  }
   /**
    * @returns {RecursiveIterator}
    */
-  [Symbol.iterator]() {
-        return this;
+  [Symbol.iterator] () {
+    return this
   }
 }
 
-
-module.exports = RecursiveIterator;
+module.exports = RecursiveIterator
--- src/lang.js
+++ src/lang.js
@@ -1,30 +1,30 @@
-'use strict';
+'use strict'
 /**
  * @param {*} any
  * @returns {Boolean}
  */
-function isObject(any) {
-  return any !== null && typeof any === 'object';
+function isObject (any) {
+  return any !== null && typeof any === 'object'
 }
 /**
  * @param {*} any
  * @returns {Boolean}
  */
-const {isArray} = Array;
+const {isArray} = Array
 /**
  * @param {*} any
  * @returns {Boolean}
  */
-function isArrayLike(any) {
-  if (!isObject(any)) return false;
-  if(!('length' in any)) return false;
-  let length = any.length;
-  if (!isNumber(length)) return false;
+function isArrayLike (any) {
+  if (!isObject(any)) return false
+  if (!('length' in any)) return false
+  let length = any.length
+  if (!isNumber(length)) return false
   if (length > 0) {
-    return (length - 1) in any;
+    return (length - 1) in any
   } else {
-    for(let key in any) {
-      return false;
+    for (let key in any) {
+      return false
     }
   }
 }
@@ -32,33 +32,32 @@ function isArrayLike(any) {
  * @param {*} any
  * @returns {Boolean}
  */
-function isNumber(any) {
+function isNumber (any) {
   return typeof any === 'number'
 }
 /**
  * @param {Object|Array} object
  * @returns {Array<String>}
  */
-function getKeys(object) {
-  let keys_ = Object.keys(object);
+function getKeys (object) {
+  let keys_ = Object.keys(object)
   if (isArray(object)) {
     // skip sort
   } else if (isArrayLike(object)) {
-    let index = keys_.indexOf('length');
+    let index = keys_.indexOf('length')
     if (index > -1) {
-      keys_.splice(index, 1);
+      keys_.splice(index, 1)
     }
     // skip sort
-  } else { 
+  } else {
     // sort
-    keys_ = keys_.sort();
+    keys_ = keys_.sort()
   }
-  return keys_;
+  return keys_
 }
 
-
-exports.getKeys = getKeys;
-exports.isArray = isArray;
-exports.isArrayLike = isArrayLike;
-exports.isObject = isObject;
-exports.isNumber = isNumber;
+exports.getKeys = getKeys
+exports.isArray = isArray
+exports.isArrayLike = isArrayLike
+exports.isObject = isObject
+exports.isNumber = isNumber
